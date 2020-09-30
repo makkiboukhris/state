4f26749b32c91e3074f838ceda549c8c7ffe5016
makkiboukhris <makki01boukhris@gmail.com>
2020-09-30 15:51:15 +0100

    Updates

---
+++ precache-manifest.67d2719da30da4fc37027c6cc3d0e8d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a43a9b5b953602dafa7e7ef5c8d0b6cd",
+    "url": "/state/index.html"
+  },
+  {
+    "revision": "372aa4abea078f3707e1",
+    "url": "/state/static/css/main.ea817bd0.chunk.css"
+  },
+  {
+    "revision": "c87ca139fb564690e5bd",
+    "url": "/state/static/js/2.92e482f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/state/static/js/2.92e482f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "372aa4abea078f3707e1",
+    "url": "/state/static/js/main.48379159.chunk.js"
+  },
+  {
+    "revision": "8079d184367af7c794fa",
+    "url": "/state/static/js/runtime-main.890111fe.js"
+  }
+]);
